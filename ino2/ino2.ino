2752112cc2ca9d0d0d37c37673e7599218b9362b
Abdulrhman Ramadan <abdalrhman07094h@gmail.com>
2019-01-28 14:26:25 +0100

    add the SerialMp3Player to the board

--- ino2/ino2.ino
+++ ino2/ino2.ino
@@ -1,34 +1,43 @@
 // board_2
+#include "SerialMP3Player.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
  
- 
 // Connect to the WiFi
-const char* ssid = "AndroidAP9921"; //fill in the network name
-const char* password = "*******"; //fill in the password
+// mobile ssid: AndroidAP9921
+const char* ssid = "sensors"; //fill in the network name
+const char* password = "n0n53n53"; //fill in the password
 const char* mqtt_server = "influx.itu.dk"; 
  
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+//define TX and RX for Serial mp3 player
+#define TX D7
+#define RX D8
+
+SerialMP3Player mp3(RX,TX);
+
+// configuring button's press length
 float pressLength_milliSeconds = 0;
- 
 int goToLunch_milliSeconds = 100;
 int comeHere_milliSeconds = 2000;        
- 
-int buttonPin = D5;
- 
 
-// declear LED pins
+// configuring button's pin
+int buttonPin = D5;
+
+// configuring LED pins
 int const_LED_pin = D7;
 int func_ledPin = D6;
 
+//configuation for const_LED_func 
 int ledState = LOW;
 long previousMillis = 0;
 long interval = 1000;
 
+ // constant LED function 
 void const_LED_func(){
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
  
   if(currentMillis - previousMillis > interval) {
     previousMillis = currentMillis;   
@@ -39,53 +48,81 @@ void const_LED_func(){
     digitalWrite(const_LED_pin, ledState);
   }  
 }
-
+// red LED function 
 void ledFunc(){
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
-    digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
-    delay(250);
+    delay(125);
     digitalWrite(func_ledPin, LOW);
-    delay(250);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, LOW);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, LOW);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, LOW);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, LOW);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, LOW);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, HIGH);
+    delay(125);
+    digitalWrite(func_ledPin, LOW);
+    delay(125);
     digitalWrite(func_ledPin, HIGH);
     delay(3000);
     digitalWrite(func_ledPin, LOW);
 }
 
-
 void goToLunch(){
   Serial.println("goToLuch");
+  mp3PlayerFunc(1);
   ledFunc();
 }
-
 void comeHere(){
   Serial.println("come here");  
+  mp3PlayerFunc(2);
   ledFunc();
 }
 
@@ -105,7 +142,7 @@ void reconnect() {
  while (!client.connected()) {
  Serial.print("Attempting MQTT connection...");
  // Attempt to connect
- if (client.connect("arduino_2","lock", "fah6eeroMaewieg1Ahqu9kaifohSho")) {
+ if (client.connect("ino_2","lock", "fah6eeroMaewieg1Ahqu9kaifohSho")) {
   Serial.println("connected");
   // ... and subscribe to topic
   client.subscribe("topic/test");//subscribe to topic/test 
@@ -128,7 +165,10 @@ void setup()
  Serial.begin(115200);
  client.setServer(mqtt_server, 1883);
  client.setCallback(callback);
- 
+ delay(250);
+   //select sd-card for Serial mp3 player 
+ mp3.sendCommand(CMD_SEL_DEV, 0, 2); 
+ delay(250); 
 }
 void loop()
 {
@@ -137,28 +177,33 @@ void loop()
  }
  // if the devide is connected to the network start the const_LED_func
  if(client.connected()){const_LED_func();}
- buttonClicking();
- client.loop();
-}
-
-void buttonClicking(){
- while (digitalRead(buttonPin) == LOW ){ 
+ 
+  while (digitalRead(buttonPin) == LOW ){ 
     delay(100);  
     pressLength_milliSeconds = pressLength_milliSeconds + 100;   
-    
+    yield();
   }
   if (pressLength_milliSeconds >= comeHere_milliSeconds){
 
        client.publish("topic/test", "1");
-
-
   } 
   else if(pressLength_milliSeconds >= goToLunch_milliSeconds){
         client.publish("topic/test", "2");
-
-
-
   }
-  pressLength_milliSeconds = 0;  
+  pressLength_milliSeconds = 0; 
+ client.loop();
 }
 
+// mp3 player function 
+void mp3PlayerFunc(int type){
+  if(type == 1){
+     mp3.play(00000001); 
+    delay(1000);
+    mp3.stop();
+  }
+  else if (type == 2){
+    mp3.play(00000002); 
+    delay(1000);
+    mp3.stop();
+  }
+}
